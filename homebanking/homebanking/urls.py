59a25f643490cd838b4fb74052edb0ba371f02c0
Karen Lettieri <karulettieri2109@gmail.com>
2022-08-11 11:30:47 -0300

    funcionalidades

--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from login import views as login_views
-from clientes import views
 from django.conf import settings
 from django.conf.urls.static import static
 from tarjetas import views as tarjetas_views
