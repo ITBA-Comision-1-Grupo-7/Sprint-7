b66e1cbbdf18e1a76d5ae417001737d64fac0683
Florencia <sicref001@gmail.com>
2022-08-12 20:42:48 -0300

    diseño form prestamo -v 01

--- homebanking/Prestamos/form.py
+++ homebanking/Prestamos/form.py
@@ -1,10 +1,12 @@
+
+from tkinter import Widget
 from django import forms 
 from .models import Prestamos 
 import datetime
 
 class createPrestamo(forms.Form):
-    dni = forms.CharField(label="DNI", required=True)
-    apellido =forms.CharField(label="Apellido", required= True)
+    dni = forms.CharField(label="DNI",widget=forms.TextInput(attrs={'class': 'form-control'}) ,required=True)
+    apellido =forms.CharField(label="Apellido",widget=forms.TextInput(attrs={'class': 'form-control'}), required= True)
     fecha_inicio = forms.DateField(initial=datetime.date.today, widget = forms.HiddenInput())
     PRESTAMO_PERSONAL = "PP"
     PRESTAMO_ESTUDIANTE = "PE"
@@ -16,7 +18,8 @@ class createPrestamo(forms.Form):
       (PRESTAMO_COMERCIOS,"Prestamo_Comercios"),
       (PRESTAMO_HIPOTECARIO, "Prestamo_Hipotecario"),
     ] 
-    valor = forms.CharField(label='Valor del prestamo', initial=0, required= True)
-    tipo_prestamo = forms.CharField(label='Tipo de prestamo',widget=forms.Select(choices=TIPO_PRESTAMO), initial=PRESTAMO_PERSONAL ,required=True)
-    estado_prestamo= forms.BooleanField(label = 'Estado', initial= True)
+    valor = forms.CharField(label='Valor del prestamo',widget=forms.TextInput(attrs={'class': 'form-control'}), required= True)
+    tipo_prestamo = forms.CharField(label='Tipo de prestamo',widget=forms.Select(choices=TIPO_PRESTAMO,attrs={'class': 'form-control'}), initial=PRESTAMO_PERSONAL ,required=True)
+    estado_prestamo= forms.BooleanField(label= 'Estado',disabled=True,initial= True)
     
+   
