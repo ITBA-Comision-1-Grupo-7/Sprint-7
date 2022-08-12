dd0ac4c5b2337c5284783d776295c733ac959d55
djseeba173 <djseeba173@gmail.com>
2022-08-12 12:02:01 -0300

    formulaarios funcionales, se suben a la base de datos, y chequea el tipo de cliente para aprobar el prestamo o no

--- homebanking/Prestamos/views.py
+++ homebanking/Prestamos/views.py
@@ -1,6 +1,9 @@
+from ast import Num
 from django.shortcuts import render
 from .form import createPrestamo
 from .models import Prestamos
+import sqlite3
+
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -10,6 +13,7 @@ from django.contrib.auth.decorators import login_required
 # @login_required
 def prestamo(request): 
     contact_form = createPrestamo
+    todoBien = False
     if request.method == "POST":
         #Traemos los datos enviados
         contact_form = contact_form(data=request.POST)
@@ -20,17 +24,43 @@ def prestamo(request):
             fecha_inicio = request.POST.get('fecha_inicio')
             tipo_prestamo = request.POST.get('tipo_prestamo')
             estado = request.POST.get('estado_prestamo')
-            valor = request.POST.get('valor')
-           
-            misPrestamos = Clientes.objects.filter(dni = dni)
-            
-            for ele in misPrestamos:
-                print(ele.dni)
+            valor = int(request.POST.get('valor'))
             if estado == 'on':
                 estado = True
             else:
                 estado = False
-            prestamo = Prestamos(dni= dni, apellido= apellido ,fecha_inicio= fecha_inicio, tipo_prestamo=tipo_prestamo,estado_prestamo= estado, valor = valor)
-            prestamo.save()
-            return render(request,'prestamos/prestamos.html',{'enviado': apellido})
+           
+            sqliteconnection = sqlite3.connect('db.sqlite3')
+            
+            cursor = sqliteconnection.cursor()
+            cursor.execute('SELECT customer_id FROM cliente WHERE customer_DNI = ' + dni)
+            customer_id = cursor.fetchall()
+            customer_id = str(customer_id[0][0])
+            cursor.execute('SELECT account_type_id FROM cuenta WHERE customer_id = ' + customer_id)
+            account_type = cursor.fetchall()
+            account_type= str(account_type[0][0])
+            
+            if account_type == '1':
+                if valor <= 100000:
+                    todoBien = True
+            elif account_type == '2':
+                if valor <= 300000:
+                    todoBien = True
+            elif account_type == '3':
+                if valor <= 500000:
+                    todoBien = True
+            
+            if todoBien:
+                prestamo = Prestamos(dni= dni, apellido= apellido ,fecha_inicio= fecha_inicio, tipo_prestamo=tipo_prestamo,estado_prestamo= estado, valor = valor)
+                prestamo.save()
+                return render(request,'prestamos/prestamos.html',{'enviado': apellido, 'flag':todoBien})
+            else:
+                return render(request,'prestamos/prestamos.html',{'enviado': apellido, 'flag':todoBien})
+                
+            
+                
+                
+            
+                
+                
     return render(request,'prestamos/prestamos.html',{'form': contact_form})
