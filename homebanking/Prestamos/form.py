1573853ef60e06945d88d93512d9f25107680809
djseeba173 <djseeba173@gmail.com>
2022-08-12 01:07:40 -0300

    Viendo algunas cosas de gramatica y css del formulario de prestamo

--- homebanking/Prestamos/form.py
+++ homebanking/Prestamos/form.py
@@ -11,12 +11,12 @@ class createPrestamo(forms.Form):
     PRESTAMO_COMERCIOS = "PC"
     PRESTAMO_HIPOTECARIO= "PH"
     TIPO_PRESTAMO = [
-      (PRESTAMO_PERSONAL,"Prestamo_personal"),
-      (PRESTAMO_ESTUDIANTE,"Prestamo_estudiante"),
-      (PRESTAMO_COMERCIOS,"Prestamo_comercios"),
-      (PRESTAMO_HIPOTECARIO, "Prestamo_hipotecario"),
+      (PRESTAMO_PERSONAL,"Prestamo_Personal"),
+      (PRESTAMO_ESTUDIANTE,"Prestamo_Estudiante"),
+      (PRESTAMO_COMERCIOS,"Prestamo_Comercios"),
+      (PRESTAMO_HIPOTECARIO, "Prestamo_Hipotecario"),
     ] 
     tipo_prestamo = forms.CharField(label='Tipo de prestamo',widget=forms.Select(choices=TIPO_PRESTAMO), initial=PRESTAMO_PERSONAL ,required=True)
-    estado_prestamo= forms.BooleanField(label = 'estado', initial= True)
+    estado_prestamo= forms.BooleanField(label = 'Estado', initial= True)
     
     # proyecto_id= forms.CharField(label='Que proyecto te intereso?', widget=forms.Select(choices=lista))
