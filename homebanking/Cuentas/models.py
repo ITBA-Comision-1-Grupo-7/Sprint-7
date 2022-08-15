5da0a3032a76db4d6c510a0c5d5cecd5fd92421d
gustavo martin <gamartingarcia37@gmail.com>
2022-08-15 16:07:01 -0300

    changed dni fetch

--- homebanking/Cuentas/models.py
+++ homebanking/Cuentas/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Cliente(models.Model):
     apellido:models.CharField(max_length=30)
     nombre:models.CharField(max_length=30)
-    balance:models.FloatField
+    balance:models.CharField
     tipoCuenta:models.CharField(max_length=10)
-    idCuenta:models.IntegerField
-    dni:models.IntegerField
+    idCuenta:models.CharField
+    dni:models.CharField
+    nombreCompleto:models.CharField(max_length=64)
--- homebanking/Cuentas/views.py
+++ homebanking/Cuentas/views.py
@@ -2,33 +2,38 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from .models import Cliente
+from login.forms import loginForm
 import sqlite3
 # Create your views here.
 
 def Cuenta(request):
-    #definitivamente no sé si funciona porque todo se rompe al no tener input
-    #sqliteconnection=sqlite3.connect('db.sqlite3')
-    #cursor=sqliteconnection.cursor()
-    #cursor.execute('SELECT customer_name FROM cliente WHERE customer_DNI ='+ dni)
-    # Cliente.nombre=cursor.fetchall()
-    # Cliente.nombre=str(Cliente.nombre)
-    # cursor.execute('SELECT customer_surname FROM cliente WHERE customer_DNI =' + dni)
-    # Cliente.apellido=cursor.fetchall()
-    # Cliente.apellido=str(Cliente.apellido)
-    # cursor.execute('SELECT customer_id FROM cliente WHERE customer_DNI =' + dni)
-    # Cliente.idCuenta=cursor.fetchall()
-    # Cliente.idCuentastr(Cliente.idCuenta)
-    # cursor.execute('SELECT balance FROM cuenta WHERE customer_id =' + customer_id)
-    # Cliente.balance=cursor.fetchall()
-    # Cliente.balance=str(Cliente.balance)
-    # cursor.execute('SELECT account_type_id FROM cuenta WHERE customer_id =' + customer_id)
-    # Cliente.tipoCuenta=cursor.fetchall()
-    # Cliente.tipoCuenta=str(Cliente.tipoCuenta)
+    # definitivamente no sé si funciona porque todo se rompe al no tener input
+    #  Cliente.dni=loginForm.name
+    #  sqliteconnection=sqlite3.connect('db.sqlite3')
+    
+    #  cursor=sqliteconnection.cursor()
+    #  cursor.execute('SELECT customer_name FROM cliente WHERE customer_DNI ='+ Cliente.dni)
+    #  Cliente.nombre=cursor.fetchall()
+    #  Cliente.nombre=str(Cliente.nombre)
+    #  cursor.execute('SELECT customer_surname FROM cliente WHERE customer_DNI =' + Cliente.dni)
+    #  Cliente.apellido=cursor.fetchall()
+    #  Cliente.apellido=str(Cliente.apellido)
+    #  cursor.execute('SELECT customer_id FROM cliente WHERE customer_DNI =' + Cliente.dni)
+    #  Cliente.idCuenta=cursor.fetchall()
+    #  Cliente.idCuenta=str(Cliente.idCuenta)
+    #  cursor.execute('SELECT balance FROM cuenta WHERE customer_id =' + Cliente.idCuenta)
+    #  Cliente.balance=cursor.fetchall()
+    #  Cliente.balance=str(Cliente.balance)
+    #  cursor.execute('SELECT account_type_id FROM cuenta WHERE customer_id =' + Cliente.idCuenta)
+    #  Cliente.tipoCuenta=cursor.fetchall()
+    #  Cliente.tipoCuenta=str(Cliente.tipoCuenta)
 
-    # if Cliente.tipoCuenta==1:
-    #     Cliente.tipoCuenta='Classic'
-    # if Cliente.tipoCuenta==2:
-    #     Cliente.tipoCuenta='Gold' 
-    # if Cliente.tipoCuenta==3:
-    #     Cliente.tipoCuenta='Black'
-    return render(request,"cuentas/cuentas.html",{'Cliente':Cliente})
+    #  if Cliente.tipoCuenta==1:
+    #      Cliente.tipoCuenta='Classic'
+    #  if Cliente.tipoCuenta==2:
+    #      Cliente.tipoCuenta='Gold' 
+    #  if Cliente.tipoCuenta==3:
+    #      Cliente.tipoCuenta='Black'
+    #  Cliente.nombreCompleto=Cliente.apellido + ', ' + Cliente.nombre
+
+return render(request,"cuentas/cuentas.html",{'Cliente':Cliente})
